4d4c7186fbf16b3ef21622a0645fe3f2367d3aa6
mattjkenney <86480338+mattjkenney@users.noreply.github.com>
2024-06-19 10:22:09 -0400

    Update getdatafile.py

--- getdatafile.py
+++ getdatafile.py
@@ -23,7 +23,7 @@ def get_dataframe_subset_for_sample(master_dataframe: pd.DataFrame, periods, fea
 
 def get_dataframe_from_label(label, qty=12):
 
-    st.write(os.getcwd())
+    st.write(str(os.getcwd()))
 
     with open('vibs.pk', 'rb') as filehandle:
         keyDict = pk.load(filehandle)
